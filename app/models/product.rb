eb0adb58e6d987f3559484ed6a7ce2807d2ed2f3
Luke-McShane <lukejsmcshane@gmail.com>
2020-04-22 06:02:40 +1000

    Create a Product working; price validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates_numericality_of :price
+
+    def price=(input)
+        input.delete! "$" "£" "€"
+        #input.delete!("$", "£", "€")
+        super
+    end
 end
